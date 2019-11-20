f4aa8ebf5082defb4d89a61fda117ac0e9770e03
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-20 10:14:39 +0000

    write tests for topping up oyster, code to pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,14 @@
 class Oystercard
   attr_reader :balance
 
+  NEGATIVE_TOP_UP_AMOUNT = 'cannot top up a negative amount'
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    raise NEGATIVE_TOP_UP_AMOUNT unless amount > 0
+    @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,22 @@
 require 'oystercard'
 
 RSpec.describe Oystercard do
+  let(:test_oystercard) { Oystercard.new }
   it 'at first has a balance of 0' do
     expect(subject.balance).to be 0
+  end
+
+  context 'when topping up' do
+    it 'increases the balance by £10 when topped up £10' do
+      expect { test_oystercard.top_up(10) }.to change { test_oystercard.balance }.by 10
+    end
+
+    it 'increases the balance by £6 when topped up £6' do
+      expect { test_oystercard.top_up(6) }.to change { test_oystercard.balance }.by 6
+    end
+
+    it 'cannot be topped up by a negative amount' do
+      expect { test_oystercard.top_up }.to raise_error { Oystercard::NEGATIVE_TOP_UP_AMOUNT }
+    end
   end
 end
